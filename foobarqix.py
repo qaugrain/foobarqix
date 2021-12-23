37eaf44c61bbd54344303b19f7033578cf07d7b9
qaugrain <quentin.augrain@gmail.com>
2021-12-23 21:35:46 +0100

    Step 2 impl, tests ok

--- foobarqix.py
+++ foobarqix.py
@@ -17,20 +17,22 @@ def compute(str):
     # If the number is divisible by 7, add "Qix"
     if number % 7 == 0:
         res = res + "Qix"
-
-    # For each digit 3, 5, 7, add "Foo", "Bar", "Qix" in the digits order.
-    for char in chars:
-        if char == "5":
-            res = res + "Bar"
-        if char == "3":
-            res = res + "Foo"
-        if char == "7":
-            res = res + "Qix"
-
+    
     # default
     if number % 3 != 0 and number % 5 != 0 and number % 7 != 0 and "5" not in chars and "3" not in chars and "7" not in chars:
-        res = str
-            
+        res = str.replace("0", "*")
+    else:
+        # For each digit 3, 5, 7, add "Foo", "Bar", "Qix" in the digits order.
+        for char in chars:
+            if char == "5":
+                res = res + "Bar"
+            if char == "3":
+                res = res + "Foo"
+            if char == "7":
+                res = res + "Qix"
+            if char == "0":
+                res = res + "*"
+                
     print(res)
 
 compute(sys.argv[1])
